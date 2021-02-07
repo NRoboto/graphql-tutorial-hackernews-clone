6eb4f71d9c63b365866209ec3cafa8c7674c074c
Mike <mnightingale@tutanota.com>
2021-02-07 15:20:02 +0000

    Add link CRUD operations

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -17,10 +17,11 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a hackernews clone!`,
     feed: () => links,
+    link: (_parent, args) => links.find((link) => link.id === args.id),
   },
 
   Mutation: {
-    post: (parent, args) => {
+    post: (_parent, args) => {
       const link = {
         id: `link-${idCount++}`,
         description: args.description,
@@ -28,6 +29,23 @@ const resolvers = {
       };
 
       links = [...links, link];
+      return link;
+    },
+    updateLink: (_parent, args) => {
+      const index = links.findIndex((link) => link.id === args.id);
+
+      const link = links[index];
+
+      if (args.url) link.url = args.url;
+      if (args.description) link.description = args.description;
+
+      return link;
+    },
+    deleteLink: (_parent, args) => {
+      const index = links.findIndex((link) => link.id === args.id);
+      const link = links[index];
+      links = [...links.slice(0, index), ...links.slice(index + 1)];
+
       return link;
     },
   },
