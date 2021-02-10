1110f3667622eeb1d11094ebcfd390b38e662d17
Mike <mnightingale@tutanota.com>
2021-02-10 13:29:54 +0000

    Add filter to feed

--- hackernews-node/src/resolvers/Query.js
+++ hackernews-node/src/resolvers/Query.js
@@ -1,7 +1,16 @@
 const info = () => `This is the API of a hackernews clone!`;
 
-const feed = async (_parent, _args, context) =>
-  await context.prisma.link.findMany();
+const feed = async (_parent, args, context) =>
+  await context.prisma.link.findMany({
+    where: args.filter
+      ? {
+          OR: [
+            { description: { contains: args.filter } },
+            { url: { contains: args.filter } },
+          ],
+        }
+      : {},
+  });
 
 const link = async (_parent, args, context) =>
   await context.prisma.link.findUnique({
