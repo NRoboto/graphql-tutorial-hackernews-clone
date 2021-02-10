69b1191d3e46c9119b4a1214bd0f9c08938bd9d0
Mike <mnightingale@tutanota.com>
2021-02-10 13:53:11 +0000

    Add count to feed query

--- hackernews-node/src/resolvers/Query.js
+++ hackernews-node/src/resolvers/Query.js
@@ -1,19 +1,29 @@
 const info = () => `This is the API of a hackernews clone!`;
 
-const feed = async (_parent, args, context) =>
-  await context.prisma.link.findMany({
-    where: args.filter
-      ? {
-          OR: [
-            { description: { contains: args.filter } },
-            { url: { contains: args.filter } },
-          ],
-        }
-      : {},
+const feed = async (_parent, args, context) => {
+  const where = args.filter
+    ? {
+        OR: [
+          { description: { contains: args.filter } },
+          { url: { contains: args.filter } },
+        ],
+      }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
     skip: args.skip,
     take: args.take,
     orderBy: args.orderBy,
   });
+
+  const count = await context.prisma.link.count({ where });
+
+  return {
+    links,
+    count,
+  };
+};
 
 const link = async (_parent, args, context) =>
   await context.prisma.link.findUnique({
