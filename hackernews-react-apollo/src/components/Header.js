d1f2965480df7650cdb03135dacaab161e9968cd
Mike <mnightingale@tutanota.com>
2021-02-11 13:20:48 +0000

    Add authentication

--- hackernews-react-apollo/src/components/Header.js
+++ hackernews-react-apollo/src/components/Header.js
@@ -1,6 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
+
+const Seperator = () => <div className="ml1">|</div>;
 
 const Header = () => {
+  const history = useHistory();
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
   return (
     <div className="flex pal justify-between nowrap orange">
       <div className="flex flex-fixed black">
@@ -8,10 +14,41 @@ const Header = () => {
         <Link to="/" className="ml1 no-underline black">
           new
         </Link>
-        <div className="ml1">|</div>
-        <Link to="/create" className="ml1 no-underline black">
-          submit
+        <Seperator />
+        <Link to="/top" className="ml1 no-underline black">
+          top
         </Link>
+        <Seperator />
+        <Link to="/search" className="ml1 no-underline black">
+          search
+        </Link>
+
+        {authToken && (
+          <div className="flex">
+            <Seperator />
+            <Link to="/create" className="ml1 no-underline black">
+              submit
+            </Link>
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-fixed">
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN);
+              history.push("/");
+            }}
+          >
+            logout
+          </div>
+        ) : (
+          <Link to="/login" className="ml1 no-underline black">
+            login
+          </Link>
+        )}
       </div>
     </div>
   );
